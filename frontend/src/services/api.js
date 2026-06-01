edb379f8413d56d87050089fa1b22e0a51251728
Lydiaesayas <lydiaaesayas@gmail.com>
2026-06-01 17:25:20 +0300

    Update API URL to production Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-})
 
-API.interceptors.request.use((config) => {
+const api = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api`,
+});
+
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token")
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
@@ -23,4 +24,4 @@ API.interceptors.response.use(
   }
 )
 
-export default API
+export default api
